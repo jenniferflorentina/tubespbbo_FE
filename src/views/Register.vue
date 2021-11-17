cc6b0c3b649d8b9141c4cca8a482e04be933202c
MariaVabiolla <mariavabiolla12@gmail.com>
2021-11-17 10:39:53 +0700

    fix register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,63 +1,66 @@
 <template>
-  <v-form @submit.prevent="save()">
-    <v-container class="my-15">
-      <v-row class="my-5">
-        <v-col md="6" offset-md="3">
-          <h3 class="my-3">Sign Up</h3>
-          <v-text-field
-            v-model="payload.username"
-            label="Username"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.password"
-            label="Password"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword ? 'text' : 'password'"
-            class="custom-validation"
-            outlined
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.name"
-            label="Name"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.dob"
-            label="Date of Birth"
-            append-icon="mdi-calendar"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.address"
-            label="Address"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.phone"
-            label="Phone Number"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-text-field
-            v-model="payload.email"
-            label="Email"
-            outlined
-            clearable
-          ></v-text-field>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="primary" @click="save()">Sign-Up</v-btn>
-          </v-card-actions>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+  <div>
+    <header-component />
+    <v-form @submit.prevent="save()">
+      <v-container class="my-10">
+        <v-row>
+          <v-col md="6" offset-md="3">
+            <h3 class="my-3">Sign Up</h3>
+            <v-text-field
+              v-model="payload.username"
+              label="Username"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.password"
+              label="Password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              class="custom-validation"
+              outlined
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.name"
+              label="Name"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.dob"
+              label="Date of Birth"
+              append-icon="mdi-calendar"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.address"
+              label="Address"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.phone"
+              label="Phone Number"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-text-field
+              v-model="payload.email"
+              label="Email"
+              outlined
+              clearable
+            ></v-text-field>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary" @click="save()">Sign-Up</v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -65,10 +68,14 @@ import Vue from 'vue';
 import { mapActions } from 'vuex';
 import moment from 'moment';
 import BaseService from '@/services/Base';
+import HeaderComponent from '@/components/layouts/header/HeaderUser.vue';
 import { Users } from '@/types/User';
 
 export default Vue.extend({
   name: 'Register',
+  components: {
+    HeaderComponent,
+  },
   data: () => ({
     service: new BaseService(),
     payload: {} as Users,
